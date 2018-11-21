4dcf9c7b1a21d4eb7377a0aa333068bad945cdfe
Luuk Lenders <daanekkelenkamp@hotmail.com>
2018-11-21 15:16:36 +0100

    project update

---
+++ Assets/Scripts/VRWorldDragger.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRWorldDragger : MonoBehaviour {
+    public static VRWorldDragger instance { get { return _instance; } }
+    private static VRWorldDragger _instance = null;
+
+    public float slerpBackSpeed = 5.0f;
+
+    //public Transform reviewVRTransform;
+    public Transform manipulatedTransform;
+    public WandController wandLeft;
+    public WandController wandRight;
+
+    public bool reset;
+    //public bool setToReview;
+
+    private Vector3 startPosition;
+    private Quaternion startRotation;
+
+    void Awake() {
+        if (_instance != null) {
+            Debug.LogWarning("Trying to instantiate multiple VRWorldDragger.");
+            DestroyImmediate(this.gameObject);
+        }
+        _instance = this;
+
+        startPosition = manipulatedTransform.position;
+        startRotation = manipulatedTransform.rotation;
+        reset = true;
+        //setToReview = false;
+    }
+
+    /*
+    public void SetToReview() {
+        manipulatedTransform.position = reviewVRTransform.position;
+        manipulatedTransform.rotation = reviewVRTransform.rotation;
+    }
+    */
+
+    public void Reset() {
+        manipulatedTransform.position = startPosition;
+        manipulatedTransform.rotation = startRotation;
+    }
+
+    void Update () {
+        wandLeft.UpdateDrag();
+        wandRight.UpdateDrag();
+
+        bool left = wandLeft.interacting && wandLeft.gameObject.activeSelf;
+        bool right = wandRight.interacting && wandRight.gameObject.activeSelf;
+
+        bool left_T = wandLeft.thumbPress && wandLeft.gameObject.activeSelf;
+        bool right_T = wandRight.thumbPress && wandRight.gameObject.activeSelf;
+
+        if (left && right) {
+            Vector3 prevDir = wandLeft.prevPos - wandRight.prevPos;
+            Vector3 currDir = wandLeft.transform.position - wandRight.transform.position;
+            prevDir.y = 0;
+            currDir.y = 0;
+            Quaternion rotation = Quaternion.FromToRotation(currDir, prevDir);
+            float angle;
+            Vector3 axis;
+            rotation.ToAngleAxis(out angle, out axis);
+            transform.RotateAround((wandLeft.transform.position * 0.5f + wandRight.transform.position * 0.5f), axis, angle);
+
+            transform.position -= (wandLeft.deltaPos * 0.5f + wandRight.deltaPos * 0.5f);
+
+
+        } else if (left ^ right) {
+            WandController wand = (left) ? wandLeft : wandRight;
+            transform.position -= wand.deltaPos;
+        } else if (left_T && right_T) {
+            manipulatedTransform.position = Vector3.Slerp(manipulatedTransform.position, startPosition, Time.time * slerpBackSpeed);
+            manipulatedTransform.rotation = Quaternion.Slerp(manipulatedTransform.rotation, startRotation, Time.time * slerpBackSpeed);
+        }
+        /*
+        else if (setToReview) {
+            SetToReview();
+            setToReview = false;
+        } */
+        
+        else if (reset) {
+            Reset();
+            reset = false;
+        }
+
+        wandLeft.SavePos();
+        wandRight.SavePos();
+    }
+}
---
+++ Assets/Scripts/WandController.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WandController : MonoBehaviour {
+
+    public Valve.VR.SteamVR_Behaviour_Pose pose;
+    public oi.plugin.linedrawing.LineDrawCapture lineDrawCapture;
+
+    private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+    private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+    private Valve.VR.EVRButtonId padButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad;
+
+    //Valve.VR.SteamVR_Behaviour_Pose controller;
+    //private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
+    //private SteamVR_TrackedObject trackedObj;
+
+
+    public Vector3 prevPos;
+    public Vector3 deltaPos = new Vector3();
+    public bool interacting = false;
+
+    public bool thumbPress = false;
+
+
+    // Use this for initialization
+    void Awake() {
+        //trackedObj = GetComponent<SteamVR_TrackedObject>();
+//        lineDrawCapture = GetComponentInChildren<LineDrawCapture>();
+    }
+
+
+
+    // Update is called once per frame
+    void Update() {
+        if (!pose.isActive) return;
+
+        if (Valve.VR.SteamVR_Input._default.inActions.GrabPinch.GetStateDown(pose.inputSource)) {
+            lineDrawCapture.SetButtonDown(true);
+        }
+
+        if (Valve.VR.SteamVR_Input._default.inActions.GrabPinch.GetStateUp(pose.inputSource)) {
+            lineDrawCapture.SetButtonDown(false);
+        }
+    }
+
+    public void UpdateDrag() {
+
+        if (!gameObject.activeInHierarchy) return;
+
+        if (Valve.VR.SteamVR_Input._default.inActions.GrabGrip.GetLastStateDown(pose.inputSource) && !interacting) {
+            interacting = true;
+        } else if (Valve.VR.SteamVR_Input._default.inActions.GrabGrip.GetLastStateUp(pose.inputSource) && interacting) {
+            interacting = false;
+        }
+
+        if (Valve.VR.SteamVR_Input._default.inActions.Teleport.GetLastStateDown(pose.inputSource) && !thumbPress) {
+            thumbPress = true;
+        } else if (Valve.VR.SteamVR_Input._default.inActions.Teleport.GetLastStateUp(pose.inputSource) && thumbPress) {
+            thumbPress = false;
+        }
+
+        if (interacting) {
+            deltaPos = transform.position - prevPos;
+        }
+
+    }
+
+    public void SavePos() {
+        prevPos = transform.position;
+    }
+
+    private void StartInteraction() {
+        interacting = true;
+        prevPos = transform.position;
+    }
+
+    private void EndInteraction() {
+        interacting = false;
+    }
+}
